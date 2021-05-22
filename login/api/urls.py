d7d9ebb2d672b5dae1ccc72dd7b31716124d94a3
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2021-05-22 18:04:49 +0530

    changes

--- login/api/urls.py
+++ login/api/urls.py
@@ -2,11 +2,11 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from django.urls import path
 
-from login.api.views import registration_request, logout_view
+from login.api.views import registration_request, logout_view, login_view
 
 
 urlpatterns = [
-	path('login/', obtain_auth_token, name='login'),
+	path('login/', login_view.as_view(), name='login'),
 	path('register/', registration_request.as_view(), name='register'),
 	path('logout/', logout_view.as_view(), name='logout_view'),
 ]
--- login/api/views.py
+++ login/api/views.py
@@ -5,7 +5,10 @@ from rest_framework.authtoken.models import Token
 from rest_framework import status
 from login import models
 from rest_framework.views import APIView
+from django.contrib.auth.models import User
 
+
+from django.contrib.auth.hashers import check_password
 
 class registration_request(APIView):
 
@@ -25,6 +28,30 @@ class registration_request(APIView):
 		else:
 			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class login_view(APIView):
+	def post(self, request):
+		username = request.data.get('username')
+		password = request.data.get('password')
+
+
+		try:
+			user_object = User.objects.get(username=username)
+		except:
+			return Response({"detail" : "username is wrong"}, status=status.HTTP_400_BAD_REQUEST)
+		
+		if check_password(password, user_object.password):
+		
+			try:
+				token = Token.objects.get(user=user_object).key
+
+				data = {}
+				data['token'] = token
+
+				return Response(data, status=status.HTTP_200_OK)			
+			except:
+				return Response({'detail' : 'login failed'}, status=status.HTTP_400_BAD_REQUEST)
+		else:
+			return Response({"detail" : "password is wrong"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class logout_view(APIView):
