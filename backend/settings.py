059c556e4bba2ec933fad1cbb3fedb0a9ac6c9b7
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2021-05-22 17:09:53 +0530

    changes

--- backend/settings.py
+++ backend/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'login',
+    'posts',
     'rest_framework.authtoken',
     'django.contrib.sites',
 
