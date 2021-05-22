d5498303fd55595cf5ad0a876db989a0c9debeca
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2021-05-22 19:40:38 +0530

    Changes

--- login/api/urls.py
+++ login/api/urls.py
@@ -6,7 +6,7 @@ from login.api.views import registration_request, logout_view, login_view
 
 
 urlpatterns = [
-	path('login/', login_view.as_view(), name='login'),
+	path('login/', obtain_auth_token, name='login'),
 	path('register/', registration_request.as_view(), name='register'),
 	path('logout/', logout_view.as_view(), name='logout_view'),
 ]
