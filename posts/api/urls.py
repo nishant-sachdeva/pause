7a2dc16a79436afdaeccbed5a8dd49e1b9a446a9
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2021-05-23 15:07:36 +0530

    simple mistake

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -2,7 +2,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from django.urls import path
 
-from posts.api.views import PostListAV, PostDetailAV, 
+from posts.api.views import PostListAV, PostDetailAV, CreatPostAV
 
 
 urlpatterns = [
